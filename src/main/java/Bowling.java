4dc84f76362fb5ed298d0cb5508e3566374b533e
Nathaniel Jerald Bea <nathanieljeraldbea@gmail.com>
2018-06-22 09:43:24 +0800

    Add other test and codes
    Added test cases and codes for other situations:
    - normal scores
    - spare
    - zero score

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -5,17 +5,22 @@ import java.util.Map;
 
 public class Bowling {
 
-    private List<String> scorePerFrame = new ArrayList<String>();
-    private List<String> statusPerFrame = new ArrayList<String>();
+    private List<String> scorePerRoll = new ArrayList<String>();
+    private List<String> statusPerRoll = new ArrayList<String>();
     private Map<String, Integer> replaceStrikeSpare = new HashMap<String, Integer>();
     private int totalScore;
 
 
     public Bowling(String scoreAllFrames) {
         for (int n = 0; n < scoreAllFrames.length(); n++) {
-            this.scorePerFrame.add(Character.toString(scoreAllFrames.charAt(n)));
-            this.statusPerFrame.add(checkStatus(Character.toString(scoreAllFrames.charAt(n))));
-            this.scorePerFrame.set(n, "10");
+            this.scorePerRoll.add(Character.toString(scoreAllFrames.charAt(n)));
+            this.statusPerRoll.add(checkStatus(Character.toString(scoreAllFrames.charAt(n))));
+            if (this.statusPerRoll.get(n).equals("Strike") ||  this.statusPerRoll.get(n).equals("Spare")) {
+                this.scorePerRoll.set(n, "10");
+            }
+            if (this.scorePerRoll.get(n).equals("-")){
+                this.scorePerRoll.set(n, "0");
+            }
         }
 
     }
@@ -24,12 +29,20 @@ public class Bowling {
         int currentScore;
         int y = 0;
         for (int n = 0; n <= 19; n++){
-            if (this.statusPerFrame.get(y) == "Strike"){
-                currentScore = Integer.valueOf(this.scorePerFrame.get(y)) + Integer.valueOf(this.scorePerFrame.get(y+1)) + Integer.valueOf(this.scorePerFrame.get(y+2));
+            if (this.statusPerRoll.get(y) == "Strike"){
+                currentScore = Integer.valueOf(this.scorePerRoll.get(y)) + Integer.valueOf(this.scorePerRoll.get(y+1)) + Integer.valueOf(this.scorePerRoll.get(y+2));
                 totalScore = totalScore + currentScore;
             n++;
-            y++;
             }
+            else if (this.statusPerRoll.get(y) == "Spare"){
+                currentScore = Integer.valueOf(this.scorePerRoll.get(y)) - Integer.valueOf(this.scorePerRoll.get(y-1)) + Integer.valueOf(this.scorePerRoll.get(y+1));
+                totalScore = totalScore + currentScore;
+            }
+            else if (this.statusPerRoll.get(y) == "Normal"){
+                currentScore = Integer.valueOf(this.scorePerRoll.get(y));
+                totalScore = totalScore + currentScore;
+            }
+            y++;
         }
 
         return totalScore;
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -8,4 +8,28 @@ public class BowlingTest {
         Bowling player = new Bowling("XXXXXXXXXXXX");
         assertEquals(player.computeScore(), 300);
     }
+
+    @Test
+    public void testScoreAllSpare() {
+        Bowling player = new Bowling("6/4/3/2/1/7/5/9/0/4/3");
+        assertEquals(player.computeScore(), 138);
+    }
+
+    @Test
+    public void testScoreAllNormal(){
+        Bowling player = new Bowling("12345123451234512345");
+        assertEquals(player.computeScore(), 60);
+    }
+
+    @Test
+    public void testHeartbreak(){
+        Bowling player = new Bowling("9-9-9-9-9-9-9-9-9-9-");
+        assertEquals(player.computeScore(),90);
+    }
+
+    @Test
+    public void testZeroEverySecondRoll(){
+        Bowling player = new Bowling("1-2-3-4-5-6-7-8-9-1-");
+        assertEquals(player.computeScore(), 46);
+    }
 }
